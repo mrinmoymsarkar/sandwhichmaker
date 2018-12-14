43ace6fd74dc86b5f54f651d10def8941ee851f6
Mrimo <tanajee.yedage@techprimelab.com>
2018-12-14 20:06:21 +0530

    First push after 112 Outputting Dynamically

--- src/containers/SandwhichMaker/SandwhichMaker.js
+++ src/containers/SandwhichMaker/SandwhichMaker.js
@@ -3,10 +3,19 @@ import  Aux from '../../hoc/Auxiliary';
 import  Sandwhich from '../../components/Sandwhich/Sandwhich'
 
 class SandwhichMaker extends Component {
+    state = {
+        ingredients: {
+            salad:1,
+            bacon:1,
+            cheese: 2,
+            meat:2
+        }
+    }
+
    render() {
        return (
            <Aux>
-               <Sandwhich/>
+               <Sandwhich ingredients = {this.state.ingredients}/>
                <div>Build Controls</div>
            </Aux>
        )
