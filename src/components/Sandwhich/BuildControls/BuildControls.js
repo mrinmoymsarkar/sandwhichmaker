e21fa3fa3801e9e404d1221fa122c40ee730b075
Mrimo <tanajee.yedage@techprimelab.com>
2018-12-18 19:14:09 +0530

    after 118 Displaying & Updating Prices

--- src/components/Sandwhich/BuildControls/BuildControls.js
+++ src/components/Sandwhich/BuildControls/BuildControls.js
@@ -21,6 +21,10 @@ const buildControls = (props) => (
                 removed={() => props.ingredientRemoved(ctrl.type) }
                 disabled = {props.disabled[ctrl.type]}/>
             ))}
+            <button className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>
+                ORDER NOW! </button>
 
     </div>
     )
--- src/containers/SandwhichMaker/SandwhichMaker.js
+++ src/containers/SandwhichMaker/SandwhichMaker.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import  Aux from '../../hoc/Auxiliary';
 import  Sandwhich from '../../components/Sandwhich/Sandwhich'
 import BuildControls from '../../components/Sandwhich/BuildControls/BuildControls'
+import  Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Sandwhich/OrderSummary/OrderSummary'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -18,8 +20,25 @@ class SandwhichMaker extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false,
+        purchasing:false
     }
+
+    updatePurchaseState (ingredients) {
+        // const ingredients = {
+        //     ...this.state.ingredients
+        // };
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({purchasable: sum >0})
+    }
+
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type]
@@ -32,6 +51,14 @@ class SandwhichMaker extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients)
+    }
+
+    purchaseHandler= () => {
+        this.setState({purchasing: true})
+    }
+    purchaseCancelHandler = () => {
+        this.setState({purchasing:false})
     }
 
     removeIngredientHandler = (type) => {
@@ -48,6 +75,7 @@ class SandwhichMaker extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients)
     }
 
 
@@ -61,11 +89,16 @@ class SandwhichMaker extends Component {
 
         return (
             <Aux>
+                <Modal show = {this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
+                    <OrderSummary ingredients={this.state.ingredients  }/>
+                </Modal>
                 <Sandwhich ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled = {disabledInfo}
+                    purchasable = {this.state.purchasable}
+                    ordered = {this.purchaseHandler}
                     price = {this.state.totalPrice}/>
 
     </Aux>
